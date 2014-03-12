bf9849cee60c696d7aa41f712491de260d082feb
Damir Zekic <damirz@gmail.com>
2014-03-12 01:07:01 +0100

    Scaffold rooms resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RoomReservations::Application.routes.draw do
+  resources :rooms
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
